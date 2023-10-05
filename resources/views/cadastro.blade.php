985ea1c423007cbfebad9dd2b9f0288febabf9bc
LeonardoHq1 <leohquinalha13@gmail.com>
2023-10-05 09:24:42 -0300

    tela de cadastro

---
+++ resources/views/cadastro.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.7.7/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    
+    <title>Lista de usuários</title>
+</head>
+<body>
+    <h1 class="w-full text-center text-xl font-bold">Cadastro</h1>
+<form action="" method="post">
+
+
+<div class="form-control">
+          <label class="label">
+            <span class="label-text">Usuario</span>
+          </label>
+          <input type="text" placeholder="Usuario" class="input input-bordered" required />
+        </div>
+
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Nome</span>
+          </label>
+          <input type="text" placeholder="Nome" class="input input-bordered" required />
+        </div>
+
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Bio</span>
+          </label>
+          <input type="text" placeholder="Bio" class="input input-bordered" required />
+        </div>
+
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="Email" class="input input-bordered" required />
+        </div>
+
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Senha</span>
+          </label>
+          <input type="text" placeholder="Senha" class="input input-bordered" required />
+        </div>
+
+
+
+    </form>
+  
+
+    
+</body>
+</html>
